8f037680b25f7909324d1ef039cf2652b85929b3
David Lyons <lyons_ccl@hotmail.com>
2022-04-08 12:51:49 +0100

    Replaced .T. with 1 and .F. with 0

--- check_dan/module1.vb
+++ check_dan/module1.vb
@@ -10,9 +10,30 @@ Module Module1
     Sub Main(ByVal feed() As String)
         Dim command As String = feed(0)
 
+        '// For BULK update ////////////////////
 
-        'newValue = "108.99"
-        'au = "00196P01"
+        '  Dim missing, line As String
+        '  missing = "C:\David\WIP\missing.txt"
+        '  FileOpen(50, missing, OpenMode.Input, OpenAccess.Read)
+        '  Do Until EOF(50)
+        '  line = LineInput(50)
+        '  If line.Length > 10 Then
+        '  Try
+        '  updateDev(line)
+        '  Catch
+        '  End Try
+        '  End If
+        '  Loop
+        '  FileClose(50)
+        '
+        '/////////////////////////////
+
+        ' command = "INSERT INTO tiff_status (time_stamp, order_num, au_num, del_date, pool, thickness, surface, order_date, work_days, downloaded, del_ori, mask_color, large_order) VALUES (637846769847355258,'david','david-1.mht','16/03/2018','1','1_00mm_material','3','09/03/2018','6',1,'16/03/2018','GREEN','0')"
+
+        'command = "INSERT INTO tiff_status (time_stamp, order_num, au_num, del_date, pool, thickness, surface, work_days, order_date, downloaded, del_ori, rek_reason, mask_color) VALUES (637850187310943822,'12344323','12344323.mht','29/04/2022','1','1_60mm_material','3','2','09/04/2022',.T.,'29/04/2022','','GREEN')"
+
+        command = command.Replace(".T.", "1")
+        command = command.Replace(".F.", "0")
 
         Try
             updateDev(command) 'Dev
@@ -41,8 +62,7 @@ leave_sub:
         Catch e As System.Exception
             Try
                 Dim fail As StreamWriter
-                'fail = File.AppendText("D:\storage\IN_HOUSE_SOFTWARE\ALL_SOFTWARE\Visual_Studio_2010\OMS-Download\TEMP\OrdersInfoSQL_Update.txt")
-                fail = File.AppendText("T:\IN_HOUSE_SOFTWARE\ALL_SOFTWARE\Visual_Studio_2010\OMS-Download\TEMP\OrdersInfoSQL_Update.txt")
+                fail = File.AppendText("T:\IN_HOUSE_SOFTWARE\ALL_SOFTWARE\Visual_Studio_2010\OMS-Download\TEMP\OMS_Dev_SQL_Update.txt")
                 fail.WriteLine("DatabaseSQL_UpdateCannot Connect")
                 fail.WriteLine(sqlCommand.CommandText)
                 fail.WriteLine(e.Message)
@@ -57,8 +77,7 @@ leave_sub:
             Catch
                 sqlConnection.Close()
                 Dim fail2 As StreamWriter
-                'fail2 = File.AppendText("D:\storage\IN_HOUSE_SOFTWARE\ALL_SOFTWARE\Visual_Studio_2010\OMS-Download\TEMP\OrdersInfoSQL_Update.txt")
-                fail2 = File.AppendText("T:\IN_HOUSE_SOFTWARE\ALL_SOFTWARE\Visual_Studio_2010\OMS-Download\TEMP\OrdersInfoSQL_Update.txt")
+                fail2 = File.AppendText("T:\IN_HOUSE_SOFTWARE\ALL_SOFTWARE\Visual_Studio_2010\OMS-Download\TEMP\OMS_Dev_SQL_Update.txt")
                 fail2.WriteLine("DatabaseSQL_Update Cannot Execute")
                 fail2.WriteLine(sqlCommand.CommandText)
                 fail2.Close()
